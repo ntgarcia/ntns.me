0a98349edef72cbd701ffd6021363dccdbb5577c
Nathan Garcia <n8g423@gmail.com>
2024-01-04 03:02:54 -0700

    Update page.tsx

--- app/(frontpage)/page.tsx
+++ app/(frontpage)/page.tsx
@@ -4,10 +4,14 @@ import FeatureCards from "../../components/pages/projects";
 import Overview from "../../components/pages/overview";
 import OpenSource from "../../components/pages/opensource";
 import Footer from "../../components/layout/footer";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <main>
+      <Head>
+        <title>Nathan Garcia</title>
+      </Head>
       <Hero />
       <FeatureCards />
       <Footer />
