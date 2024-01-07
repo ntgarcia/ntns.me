881f3e75e8f4501adce8e86d1f7ab29cd592c2db
Nathan Garcia <n8g423@gmail.com>
2024-01-06 21:29:14 -0700

    Update problem.tsx

--- components/pages/projects/boga/problem.tsx
+++ components/pages/projects/boga/problem.tsx
@@ -1,11 +1,5 @@
 "use client";
 
-import { ImageFrame } from "../image-frame";
-import Image from "next/image";
-import { siteConfig } from "../../config/site";
-import Link from "next/link";
-import { motion } from "framer-motion";
-
 export default function Problem() {
   return (
     <section className="bg-secondary">
